7bba8885e39bd6881f206e95e4604e6596a7638d
Guido Halley <49423495+guidohalley@users.noreply.github.com>
2021-02-22 09:12:20 -0300

    Febrero 22

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,11 +2,29 @@
 
 @section('content')
     @include('layouts.headers.cards')
- 
-    
-
+        <div class="container">
+            <h1 class="display-1">Listado de Admitidos</h1>
+            <div class="header-body">                
+                <!-- Tabla  --> 
+                <div class="table-responsive">
+                    <table class="table align-items-center table-dark">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Nombre </th>
+                                <th scope="col">Radicación</th>
+                                <th scope="col">Asistencia </th>
+                                <th scope="col">Número de Expediente</th>
+                                <th scope="col">Estado de Salud</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                    </table>    
+                </div>
+            </div>
+        </div>          
     @include('layouts.footers.auth')
-    </div>
+    
 @endsection
 
 @push('js')
--- resources/views/layouts/headers/cards.blade.php
+++ resources/views/layouts/headers/cards.blade.php
@@ -1,28 +1,31 @@
-<div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+
+
+<div class="header bg-gradient-primary pb-3 pt-5 pt-md-5">
     <div class="container-fluid">
-        <div class="header-body">
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-9 col-xs-9">
-                <!-- Card stats -->            
-                <button type="button" class="btn btn-secondary btn-lg btn-block" >
-                    <h5 class="card-title text-uppercase text-muted mb-0">Posadas</h5>
-                    <span class="h2 font-weight-bold mb-0">0</span>
-                </button>
-
-                <button type="button" class="btn btn-secondary btn-lg btn-block">
-                    <h5 class="card-title text-uppercase text-muted mb-0">Iguazú</h5>
-                    <span class="h2 font-weight-bold mb-0">0</span>
-                </button>  
-
-                <button type="button" class="btn btn-secondary btn-lg btn-block">
-                    <h5 class="card-title text-uppercase text-muted mb-0">Oberá</h5>
-                    <span class="h2 font-weight-bold mb-0">0</span>
-                </button>
-                <br class="table-responsive">   
-                <!-- Tabla  --> 
-                            
-
+        <div class="header bg-gradient-primary pb-3 pt-5 pt-md-5">
+            <div class="container-fluid">
+                <div class="header-body">
+                    <div class="row">    
+                        <!-- Cards de cantidad de admisiones segun ciudad. -->            
+                        <button type="button" class="btn btn-secondary btn-lg active" >
+                            <h5 class="card-title text-uppercase text-muted mb-0">Posadas</h5>
+                            <span class="h2 font-weight-bold mb-0">0</span>
+                        </button>
+    
+                        <button type="button" class="btn btn-secondary btn-lg active">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Iguazú</h5>
+                            <span class="h2 font-weight-bold mb-0">0</span>
+                        </button>  
+    
+                        <button type="button" class="btn btn-secondary btn-lg active">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Oberá</h5>
+                            <span class="h2 font-weight-bold mb-0">0</span>
+                        </button>
+                        <br class="table-responsive">   
+                        <!-- Tabla  --> 
+                    </div>
+                </div>
             </div>
-     
-        </div>
+        </div>       
     </div>
 </div>
